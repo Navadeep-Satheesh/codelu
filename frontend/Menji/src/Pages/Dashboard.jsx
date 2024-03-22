ff7e403be2af51d0a8de918da77e1080d2e06186
Ski <skicodeoo7@gmail.com>
2024-03-23 02:14:32 +0530

    desr

--- frontend/Menji/src/Pages/Dashboard.jsx
+++ frontend/Menji/src/Pages/Dashboard.jsx
@@ -1,13 +1,38 @@
-
+import Bitmoji from "../assets/Images/Bitmoji.png";
+import "./Dash_style.css";
 const Dashboard = () => {
   return (
-    <div className="Dashboard-body">
-        <div className="red-container">
-            <div className="black-container">
-                <img src="../assets/Bitmoji.png" alt="Avatar" className="bitimage" />
+    <div className="Parent">
+        <div className="left">
+            <div className="task">
+                Task1
+            </div>
+            <div className="task">
+                Task2
+            </div>
+            <div className="task">
+                Task3
+            </div>
+
+        </div>
+        <div className="side-contents">
+            <div className="red-container">
+                <img src={Bitmoji} alt="Logo" className="Chechi" />
+            </div>
+            <div className="details">
+                {/* <p>User-Name : Ski<br/>Email-id:ijewej@gmaol.com<br/>Exp : 190<br/>Level:10</p> */}
+                <span>User-Name: Ski</span>
+                <span>Email: ijewej@gmaol.com</span>
+                <span>Exp : 190</span>
+                <span>Level:10</span>
+
+            </div>
+            <div className="Missions">
+                <span className="Missions">Missions</span>
+                <li className="myMission">Front-End Development</li>
+                <li className="myMission">Back-End Development</li>
             </div>
         </div>
-      
     </div>
   )
 }
