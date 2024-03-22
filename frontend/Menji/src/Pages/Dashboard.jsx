f5d232f8739a1f64ebc7f989e5eb810e70661a7d
navadeep <navadeepsatheesh@gmail.com>
2024-03-22 21:06:14 +0530

    fdf

---
+++ frontend/Menji/src/Pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function DashBoard(){
+
+
+    return (
+
+        <div className="dashboard">
+            this is dashboard
+        </div>
+    )
+
+
+}
+
+export default DashBoard
