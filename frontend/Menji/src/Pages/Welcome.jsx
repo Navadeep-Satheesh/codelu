5b8eccce93c7c59b11e26651b6d8d2ee71c2f8ce
navadeep <navadeepsatheesh@gmail.com>
2024-03-23 00:07:12 +0530

    fdfd

--- frontend/Menji/src/Pages/Welcome.jsx
+++ frontend/Menji/src/Pages/Welcome.jsx
@@ -1,34 +1,65 @@
-import React  from "react";
+import React from "react";
 import "../css/welcome.css"
+import { useState } from "react";
 
-function Welcome(){
+function Welcome() {
 
-    // let [ signInPage , setSignInPage] =  useState("")
+    let [signInPage, setSignInPage] = useState(true);
 
     return (
 
         <>
 
-        <div className="welcome">
+            <div className="welcome">
 
                 <div className="welcome_topbar">
 
                     <h3>MINDMENTOR</h3>
 
-                    <button className="welcome_signin">SIGN IN</button>
+                    <button className="welcome_signin" onClick={() => {
+                        setSignInPage(true);
+                    }}>SIGN IN</button>
 
                 </div>
 
-                <div className="welcome_main">
+                <div className="welcome_main" >
 
                     <h1>MIND MENTOR</h1>
 
                 </div>
+
+                {(signInPage) && <div className="signInPage">
+
+                        <div className="sign_in_container">
+
+                            <form action="/api/signin" method="POST">
+
+                            <h3>SIGN IN</h3>
+
+                            <input type="text" placeholder = "Username" className="email" required />
+                            <input type="text" className="password" placeholder="Password" required />
+
+                            <button className="login" >LOGIN</button>
+
+                            </form>
+
+                        </div>
+
+                </div> }
+                    
+                    
                 
-                This is welcome Page
+
+
+
+
+                    {/* </div> */}
+
+
+
 
         </div>
-            
+
         </>
     )
 }
