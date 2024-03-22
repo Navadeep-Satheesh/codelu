9d07e51412259d73cfc23c11ccb21803508fdc90
Ski <skicodeoo7@gmail.com>
2024-03-23 00:05:21 +0530

    new

---
+++ frontend/Menji/src/Pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+
+const Dashboard = () => {
+  return (
+    <div className="Dashboard-body">
+        <div className="red-container">
+            <div className="black-container">
+                <img src="../assets/Bitmoji.png" alt="Avatar" className="bitimage" />
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Dashboard
