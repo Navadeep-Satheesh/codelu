07ead1067ba09faa041d6f76c932bf6f10b02f95
Renjith312 <darkknight312137@gmail.com>
2024-03-23 07:19:06 +0530

    312 2

--- frontend/Menji/src/Pages/Community.jsx
+++ frontend/Menji/src/Pages/Community.jsx
@@ -1,11 +1,16 @@
 
 const Community = () => {
     return (
-      <div>
-        <h1>Communicate</h1>
+      <div className="main">
+        <h1>Community</h1>
+        <div className="right">
+          <span>comnnunity namelsit</span>
+          <button className="b1" onClick={alert("cbout to delete")}>chat</button>
+          <button className="b2" onClick={alert("add")}>add</button>
+        </div>
       </div>
     )
   }
   
-  export default Community
+  export default AskUs
   
