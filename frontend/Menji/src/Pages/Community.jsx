631cb9e7c40b965afbbc11e2d00aba076b0a1b18
Ski <skicodeoo7@gmail.com>
2024-03-23 17:07:03 +0530

    rw

--- frontend/Menji/src/Pages/Community.jsx
+++ frontend/Menji/src/Pages/Community.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Friends from './Friends'
+import { Outlet } from 'react-router-dom';
 function Community() {
   return (
     <div>
       <h1>hjvkyjfv</h1>
       <Friends/>
+      <Outlet />
     </div>
   )
 }
